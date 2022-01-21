2eccd61facac5a73c8963411c8995a607c565ba4
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-21 16:49:42 -0300

    Creacion contenido base en componente Sidebar

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <section className="sideBarContainer">
+      Sidebar
+    </section>
+  );
+}
+
+export default Sidebar;
