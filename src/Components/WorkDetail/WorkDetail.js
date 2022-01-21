4fc79c338c92c9e79cda8c9bbf4ad07cb0e671b1
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-21 16:51:25 -0300

    Creacion contenido base en componente workDetail

---
+++ src/Components/WorkDetail/WorkDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WorkDetail.css';
+
+function WorkDetail() {
+  return (
+    <section className="workDetailContainer">
+      WorkDetail
+    </section>
+  );
+}
+
+export default WorkDetail;
