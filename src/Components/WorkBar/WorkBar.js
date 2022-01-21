ed5a010a08636a75ae01d2875539f53869fbf12e
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-21 16:50:56 -0300

    Creacion contenido base en componente WorkBar

---
+++ src/Components/WorkBar/WorkBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WorkBar.css';
+
+function WorkBar() {
+  return (
+    <section className="workBarContainer">
+      WorkBar
+    </section>
+  );
+}
+
+export default WorkBar;
