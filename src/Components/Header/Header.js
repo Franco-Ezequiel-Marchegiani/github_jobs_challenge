2935c30f0993f05e5ff112b302354c4b15f96901
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-21 17:26:45 -0300

    Vinculé el fontawesome

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Header.css';
-
+import BackgroundImage from '../../Images/backgroundImg.png'
 function Header() {
   return (
     <header className="headerContainer">
-      Header
+        <div>
+            <input type="text" className='inputSearchHeader'/>
+            <button className='btn-searchHeader'>Search</button>
+        </div>
     </header>
   );
 }
