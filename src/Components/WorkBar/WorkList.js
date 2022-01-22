7c0fc2c4ac29334100b43ec677e8916e6a2864d1
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-22 20:15:07 -0300

    Coloqué contenido paginación

--- src/Components/WorkBar/WorkList.js
+++ src/Components/WorkBar/WorkList.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './WorkBar.css';
 import WorkBar from './WorkBar'
+import { Pagination } from 'react-bootstrap';
 function WorkList() {
   return (
     <section className="workListContainer">
-      <WorkBar/>
-      <WorkBar/>
-      <WorkBar/>
-      <WorkBar/>
-      <WorkBar/>
+        <WorkBar/>
+        <WorkBar/>
+        <WorkBar/>
+        <WorkBar/>
+        <WorkBar/>
+        <div className='paginationContainer'>
+              <Pagination>
+                <Pagination.First />
+                <Pagination.Prev />
+                <Pagination.Item>{1}</Pagination.Item>
+                <Pagination.Ellipsis />
+
+                <Pagination.Item>{10}</Pagination.Item>
+                <Pagination.Item>{11}</Pagination.Item>
+                <Pagination.Item active>{12}</Pagination.Item>
+                <Pagination.Item>{13}</Pagination.Item>
+                <Pagination.Item disabled>{14}</Pagination.Item>
+
+                <Pagination.Ellipsis />
+                <Pagination.Item>{20}</Pagination.Item>
+                <Pagination.Next />
+                <Pagination.Last />
+              </Pagination>
+        </div>
     </section>
   );
 }
