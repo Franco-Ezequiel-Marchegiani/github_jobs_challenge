af6bb7e2691d8bfc8ec02bc192f1f2af93874200
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-22 20:26:41 -0300

    Adaptation pagination

--- src/Components/WorkBar/WorkList.js
+++ src/Components/WorkBar/WorkList.js
@@ -11,21 +11,15 @@ function WorkList() {
         <WorkBar/>
         <WorkBar/>
               <Pagination className='paginationContainer'>
-                <Pagination.First />
                 <Pagination.Prev />
                 <Pagination.Item>{1}</Pagination.Item>
-                <Pagination.Ellipsis />
 
+                <Pagination.Item active>{2}</Pagination.Item>
+                <Pagination.Item>{3}</Pagination.Item>
+
+                <Pagination.Ellipsis />
                 <Pagination.Item>{10}</Pagination.Item>
-                <Pagination.Item>{11}</Pagination.Item>
-                <Pagination.Item active>{12}</Pagination.Item>
-                <Pagination.Item>{13}</Pagination.Item>
-                <Pagination.Item disabled>{14}</Pagination.Item>
-
-                <Pagination.Ellipsis />
-                <Pagination.Item>{20}</Pagination.Item>
                 <Pagination.Next />
-                <Pagination.Last />
               </Pagination>
     </section>
   );
