b6474f68d52d662d909e8e401f5c0c15be29b8db
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-21 23:05:41 -0300

    Estructura creacada para la version mobile de la barra de busqueda

--- src/Components/WorkBar/WorkBar.js
+++ src/Components/WorkBar/WorkBar.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import './WorkBar.css';
+import LogoEj from '../../Images/imglogoej.jpg'
 
 function WorkBar() {
   return (
     <section className="workBarContainer">
-        <main>
+        <main className='jobBarContainer'>
             <div className='workBarImgContainer'>
-              <img src="" alt="" />
+              <img src={LogoEj} alt="Logo Empresa" />
             </div>
             <div className='workMainInfoImgContainer'>
-              <p>Kasisto</p>
-              <p>Front-End Software Engineer</p>
-              <button>Full time</button>
-            </div>
-            <div className='workBarDataJobContainer'>
-              <div className='workBarCityContainer'>
-                  <i className="fas fa-globe-europe"></i>
-                  <p>New York</p>
-              </div>
-              <div className='workBarDaysContainer'>
-                  <i className="far fa-clock"></i>
-                  <p>5 days ago</p>
-              </div>
+                <p>Kasisto</p>
+                <p>Front-End Software Engineer</p>
+                <button>Full time</button>
+                  <div className='workBarDataJobContainer'>
+                    <div className='workBarCityContainer'>
+                        <i className="fas fa-globe-europe"></i>
+                        <p>New York</p>
+                    </div>
+                    <div className='workBarDaysContainer'>
+                        <i className="far fa-clock"></i>
+                        <p>5 days ago</p>
+                    </div>
+                  </div>
             </div>
         </main>
     </section>
