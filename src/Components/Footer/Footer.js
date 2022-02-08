6946a931141c1f0b240b99c7dad570e40a1706ae
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-08 15:13:36 -0300

    Creation of the structure of the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
   return (
     <footer>
-        Footer
+            <p>Created by 
+                <Link className='linkFooter' to="//github.com/Franco-Ezequiel-Marchegiani" target="_blank">Franco Ezequiel Marchegiani</Link> 
+                - 
+                <Link className='linkFooter' to="//devchallenges.io/" target="_blank" rel="noopener noreferrer">devChallenges.io</Link>
+            </p>
     </footer>
   );
 }
