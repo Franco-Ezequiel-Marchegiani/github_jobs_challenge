588e9feec2b8f7a55ac04dd5f14babd0a7226b9c
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-08 15:14:19 -0300

    Added style to the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <footer>
             <p>Created by 
-                <Link className='linkFooter' to="//github.com/Franco-Ezequiel-Marchegiani" target="_blank">Franco Ezequiel Marchegiani</Link> 
+                <Link className='linkFooter' to="//github.com/Franco-Ezequiel-Marchegiani" target="_blank">Franco Ezequiel Marchegiani </Link> 
                 - 
-                <Link className='linkFooter' to="//devchallenges.io/" target="_blank" rel="noopener noreferrer">devChallenges.io</Link>
+                <Link className='linkFooter' to="//devchallenges.io/" target="_blank" rel="noopener noreferrer"> devChallenges.io</Link>
             </p>
     </footer>
   );
