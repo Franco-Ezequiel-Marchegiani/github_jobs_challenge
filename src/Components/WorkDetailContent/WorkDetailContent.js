6954f73b407235da5b9bc3d9270901e4b2af285f
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-07 22:30:34 -0300

    I Created the logo, name and site in the work detail

--- src/Components/WorkDetailContent/WorkDetailContent.js
+++ src/Components/WorkDetailContent/WorkDetailContent.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import SidebarDetailWork from '../SidebarDetailWork/SidebarDetailWork';
 import './WorkDetailContent.css';
-
+import LogoEj from '../../Images/imglogoej.jpg'
 function WorkDetailContent() {
   return (
-    <section className="workDetailContainer">
-      <div className='infoHeader'>
-          <h1>Front-End Software Engineer</h1>
-          <button>Full time</button>
-          <span>5 days ago</span>
-      </div>
-    </section>
+        <section className="workDetailContainer">
+                <div className='infoHeader'>
+                    <h1>Front-End Software Engineer</h1>
+                    <button>Full time</button>
+                    <span>5 days ago</span>
+                </div>
+                <div>
+                    <img src={LogoEj} alt="" />
+                    <p>Kasisto</p>
+                    <span>New York</span>
+                </div>
+        </section>
   );
 }
 
