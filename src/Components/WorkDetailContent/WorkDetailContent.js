40ef59484e721e2e6514f913ed6fb597193d96fb
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-07 22:11:25 -0300

    Creation of the WorkDetailContent component, which it's gonna has the content of the work detail

---
+++ src/Components/WorkDetailContent/WorkDetailContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SidebarDetailWork from '../SidebarDetailWork/SidebarDetailWork';
+import './WorkDetailContent.css';
+
+function WorkDetailContent() {
+  return (
+    <section className="workDetailContainer">
+      <SidebarDetailWork/>
+    </section>
+  );
+}
+
+export default WorkDetailContent;
