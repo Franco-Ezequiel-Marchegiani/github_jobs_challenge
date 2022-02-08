2de07ea70703261c8b62c4e461d3a22964935ecb
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-07 22:13:53 -0300

    Added the component workDetailContent on the workDetail's file

--- src/Components/WorkDetail/WorkDetail.js
+++ src/Components/WorkDetail/WorkDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SidebarDetailWork from '../SidebarDetailWork/SidebarDetailWork';
+import WorkDetailContent from '../WorkDetailContent/WorkDetailContent';
 import './WorkDetail.css';
 
 function WorkDetail() {
   return (
     <section className="workDetailContainer">
       <SidebarDetailWork/>
+      <WorkDetailContent/>
     </section>
   );
 }
