aa162627fe7ece59a88b6a18676ee3de40767df7
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-08 15:31:25 -0300

    Now the work bar are links, and they redirect to the work detail view

--- src/Components/WorkBar/WorkBar.js
+++ src/Components/WorkBar/WorkBar.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import './WorkBar.css';
 import LogoEj from '../../Images/imglogoej.jpg'
+import { Link } from 'react-router-dom';
 
 function WorkBar() {
   return (
     <section className="workBarContainer">
-        <main className='jobBarContainer'>
-            <div className='workBarImgContainer'>
-              <img src={LogoEj} alt="Logo Empresa" />
-            </div>
-            <div className='workMainInfoImgContainer'>
-                <p className='titleEmpresa'>Kasisto</p>
-                <p className='titlePuesto'>Front-End Software Engineer</p>
-                <button>Full time</button>
-                  <div className='workBarDataJobContainer'>
-                    <div className='workBarCityContainer'>
-                        <i className="fas fa-globe-europe"></i>
-                        <p>New York</p>
-                    </div>
-                    <div className='workBarDaysContainer'>
-                        <i className="far fa-clock"></i>
-                        <p>5 days ago</p>
-                    </div>
-                  </div>
-            </div>
-        </main>
+        <Link className='test' to="/workDetail">
+          <main className='jobBarContainer'>
+                <div className='workBarImgContainer'>
+                  <img src={LogoEj} alt="Logo Empresa" />
+                </div>
+                <div className='workMainInfoImgContainer'>
+                    <p className='titleEmpresa'>Kasisto</p>
+                    <p className='titlePuesto'>Front-End Software Engineer</p>
+                    <button>Full time</button>
+                      <div className='workBarDataJobContainer'>
+                        <div className='workBarCityContainer'>
+                            <i className="fas fa-globe-europe"></i>
+                            <p>New York</p>
+                        </div>
+                        <div className='workBarDaysContainer'>
+                            <i className="far fa-clock"></i>
+                            <p>5 days ago</p>
+                        </div>
+                      </div>
+                </div>
+          </main>
+        </Link>
     </section>
   );
 }
