aee1b7a41e0fcd8d178631dca5fd12c1702ff703
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-09 21:43:14 -0300

    Add the logic from bootstrap

--- src/Components/WorkBar/PaginationWork.js
+++ src/Components/WorkBar/PaginationWork.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './WorkBar.css';
 import { Pagination } from 'react-bootstrap';
 function PaginationWork() {
-    
+    let active = 2;
+    let items = [];
+    for (let number = 1; number <= 5; number++) {
+    items.push(
+        <Pagination.Item key={number} active={number === active}>
+        {number}
+        </Pagination.Item>,
+    );
+    }
   return (
         <div>
             <Pagination>{items}</Pagination>
