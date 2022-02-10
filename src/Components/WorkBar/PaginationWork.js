b04c33ade676d54b8ac916474eef2b2304ed2325
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-10 19:22:55 -0300

    Clear the commits doned

--- src/Components/WorkBar/PaginationWork.js
+++ src/Components/WorkBar/PaginationWork.js
@@ -12,19 +12,12 @@ function PaginationWork({paginaActual, postTotales, publicacionesPorPagina, pagi
         pageNumbers.push(numero);
         }
     }
-    //Last steps:
-    /* 
-    - Add the Bootstrap link and script, and write the structure manualy
-    -Then, keep working with the logic, adding an event and passing the result of the event to the father
-    OPTIONA:
-    -Write the structure to made the complete pagination
-     */
   return (
         <div>
             <Pagination className={postTotales === undefined ? "paginationContainer hiddenPagination" : "paginationContainer"}>
                 {paginaActual === 1 ? <Pagination.First disabled /> : <Pagination.First /> }
                 {paginaActual === 1 ? <Pagination.Prev disabled /> : <Pagination.Prev /> }
-                    {paginaActual === 1 ? "" : <Pagination.Ellipsis />}
+                    {/* {paginaActual === 1 ? "" : <Pagination.Ellipsis />} */}
                     {pageNumbers.map(numero =>{
                         return <Pagination.Item onClick={() => paginacion(numero) } key={numero} active={numero === active}>
                                     {numero}
