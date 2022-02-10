498c981cff52b2176e67448e59fc4b003cb96acf
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-09 22:14:34 -0300

    Used the values send it from the father to the child

--- src/Components/WorkBar/PaginationWork.js
+++ src/Components/WorkBar/PaginationWork.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './WorkBar.css';
 import { Pagination } from 'react-bootstrap';
 function PaginationWork({postTotales, publicacionesPorPagina}) {
-    console.log(postTotales.length);
     let active = 2;
     let pageNumbers = [];
-    for (let number = 1; number <= Math.ceil(); number++) {
-    pageNumbers.push(
-        <Pagination.Item key={number} active={number === active}>
-        {number}
-        </Pagination.Item>,
-    );
+    if(postTotales !== undefined){
+        console.log(postTotales.length);
+        
+        for (let number = 1; number <= Math.ceil(postTotales.length / publicacionesPorPagina); number++) {
+        pageNumbers.push(
+            <Pagination.Item key={number} active={number === active}>
+            {number}
+            </Pagination.Item>,
+        );
+        }
     }
   return (
         <div>
