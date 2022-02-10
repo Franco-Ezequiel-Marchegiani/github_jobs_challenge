e3ae89e66c027ec1a5f3d1a0db1a5b4896fed0c5
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-09 21:42:58 -0300

    Created a new component to put the pagination code inside

---
+++ src/Components/WorkBar/PaginationWork.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WorkBar.css';
+import { Pagination } from 'react-bootstrap';
+function PaginationWork() {
+    
+  return (
+        <div>
+            <Pagination>{items}</Pagination>
+        </div>
+  );
+}
+
+export default PaginationWork;
