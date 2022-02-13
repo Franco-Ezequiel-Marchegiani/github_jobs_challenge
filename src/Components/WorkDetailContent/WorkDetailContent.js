c6b295af08a96fa5fba6a444378b8ca4e20ce2f7
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-12 23:02:07 -0300

    Set the time of the job

--- src/Components/WorkDetailContent/WorkDetailContent.js
+++ src/Components/WorkDetailContent/WorkDetailContent.js
@@ -64,7 +64,7 @@ function WorkDetailContent(props) {
                    <section>
                         <div className='infoHeader'>
                                 <h1>{filtroPorId.MatchedObjectDescriptor.PositionTitle}</h1>
-                                <button>Full time</button>
+                                <button>{filtroPorId.MatchedObjectDescriptor.PositionSchedule[0].Name}</button>
                                 <span><i className="far fa-clock"></i>{diasTranscurridos} days ago</span>
                         </div>
                         <div className='infoEmpresaWorkDetail'>
