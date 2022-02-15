5d46305fbe0f2d64043cab867969a604b8ee0844
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-14 22:18:48 -0300

    Change the location on the detail

--- src/Components/WorkDetailContent/WorkDetailContent.js
+++ src/Components/WorkDetailContent/WorkDetailContent.js
@@ -116,7 +116,7 @@ function WorkDetailContent(props) {
                                         <p>Competitive compensation package Ground floor opportunity within rapidly growing tech startup Great collaborative team environment Full Benefits Fun perks</p>
                                 </div>
                                 <div className='infoLocation'>
-                                        <p>Location - NYC, Flatiron District</p>
+                                        <p>Location - {filtroPorId.MatchedObjectDescriptor.PositionLocationDisplay}</p>
                                         <p>We welcome your cover letter with a description of your previous complete experience and your resume. Applicants must be authorized to work in the US as we are unable to sponsor. Kasisto is an equal opportunity employer.</p>
                                 </div>
                         </section>
