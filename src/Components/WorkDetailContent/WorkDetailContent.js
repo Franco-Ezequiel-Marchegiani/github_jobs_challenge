fc279c5f67d57b3feb20bde76e17466b7c39485d
Franco Ezequiel <francoezequielmarchegiani2019@gmail.com>
2022-02-16 23:49:02 -0300

    Add the telephone of the agency to the fourty paragraph

--- src/Components/WorkDetailContent/WorkDetailContent.js
+++ src/Components/WorkDetailContent/WorkDetailContent.js
@@ -124,6 +124,7 @@ function WorkDetailContent(props) {
                                         <p>Location - {filtroPorId.MatchedObjectDescriptor.PositionLocationDisplay}</p>
                                         <p>You can see the benefits <Link to={filtroPorId.MatchedObjectDescriptor.UserArea.Details.BenefitsUrl}>clicking here</Link></p>
                                         <p>{filtroPorId.MatchedObjectDescriptor.UserArea.Details.AgencyContactEmail}</p>
+                                        <p>{filtroPorId.MatchedObjectDescriptor.UserArea.Details.AgencyContactPhone}</p>
                                         <p>We welcome your cover letter with a description of your previous complete experience and your resume. Applicants must be authorized to work in the US as we are unable to sponsor. Kasisto is an equal opportunity employer.</p>
                                 </div>
                         </section>
