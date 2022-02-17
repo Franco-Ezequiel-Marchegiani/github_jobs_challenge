a2e8b7eada911e84ffbbb36d9e0e6d910abe6b00
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-17 19:12:27 -0300

    test3

--- src/Components/WorkDetailContent/WorkDetailContent.js
+++ src/Components/WorkDetailContent/WorkDetailContent.js
@@ -54,7 +54,7 @@ function WorkDetailContent(props) {
             let miliSegundosTranscurridos = Math.abs(fechaPublicacion.getTime() - fechaActual.getTime());
             diasTranscurridos = Math.round(miliSegundosTranscurridos/miliSegundosDia);
        }
-        //Test2
+        //Test3
         return (
               <section className="workDetailContainer">
                       {/* Hay un error al colocar el spiner */}
