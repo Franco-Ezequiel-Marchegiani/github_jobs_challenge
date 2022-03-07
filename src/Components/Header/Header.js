8aed3e41ad446bd49a127c315b7970b8250ea770
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-07 19:03:10 -0300

    Created the logic and now the search bar from the header it's working

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
 import BackgroundImage from '../../Images/backgroundImg.png'
-function Header() {
+function Header({setUserSearch}) {
   return (
     <header className="headerContainer">
         <div className='searchHeaderContainer'>
             <i className="fas fa-briefcase"></i>
-            <input type="text" placeholder='Title, companies, expertise or benefits' className='inputSearchHeader'/>
+            <input onChange={(event)=> setUserSearch(event.target.value)} type="text" placeholder='Title, companies, expertise or benefits' className='inputSearchHeader'/>
             <button className='btn-searchHeader'>Search</button>
         </div>
     </header>
